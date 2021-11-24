cb72ffe7063de8ea43b8de9bf9e9ba7f62d7e68f
axel <axel.coudair.pro@gmail.com>
2021-11-24 22:39:51 +0100

    🚧 MintArea big WIP

---
+++ components/MintArea/MintAreaStyle.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+    countSpan: {
+        background: "#71712E",
+        width: "100px"
+    }
+}));
+
+export default useStyles;
