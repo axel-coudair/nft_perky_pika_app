c698111fa2eedb06a161fa2c8615012bb4ac3bae
axel <axel.coudair.pro@gmail.com>
2021-11-24 21:13:39 +0100

    🔧Mui Icons Configured and page mint created and nav bar done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Test from '../components/test/test'
 import Link from 'next/link'
 import { Button } from '@mui/material'
 
 export default function Home() {
   return (
     <>
-      <Test />
       <Link href="/mint">
         <Button variant="contained">Mint</Button>
       </Link>
