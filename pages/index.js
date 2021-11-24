e75c448dfba625e98e7bdcd9050dc6165025b4b6
axel <axel.coudair.pro@gmail.com>
2021-11-24 20:47:58 +0100

    ✨Routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Test from '../components/test/test'
+import Link from 'next/link'
+import { Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <Test />
+    <>
+      <Test />
+      <Link href="/mint">
+        <Button variant="contained">Mint</Button>
+      </Link>
+    </>
   )
 }
